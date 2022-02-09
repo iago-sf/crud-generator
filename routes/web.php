742e2792f3ff49e3a307d57e8561b535fc172c9e
iago <iaguerreles@gmail.com>
2022-02-09 18:29:45 +0000

    A54

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => 'verified'], function () {
+    Route::resource('customers', App\Http\Controllers\CustomerController::class);
+    Route::resource('users', App\Http\Controllers\UserController::class);
 });
